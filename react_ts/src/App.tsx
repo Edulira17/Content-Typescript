b9e9e8777a963bbf458a00e5420291c95e82334e
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-11-26 15:02:50 -0400

    Finalizando seção

--- react_ts/src/App.tsx
+++ react_ts/src/App.tsx
@@ -3,10 +3,12 @@ import FirstComponent from "./components/FirstComponent";
 
 // 5 - desestruturando props
 import SecondComponent from "./components/SecondComponent";
-import Destructuring, {Category} from "./components/Destructuring";
+import Destructuring, { Category } from "./components/Destructuring";
 
 // 6 - UseState
 import State from "./components/State";
+import { createContext } from "react";
+import Context from "./components/Context";
 
 // 9 - type
 type textOrNull = string | null;
@@ -15,14 +17,14 @@ type fixed = "Isso" | "Ou" | "Aquilo";
 
 // 10 - context
 interface IAppContext {
-  language: string
-  framework: string
-  projects: number
+  language: string;
+  framework: string;
+  projects: number;
 }
 
+export const AppContext = createContext<IAppContext | null>(null);
 
 function App() {
-
   // 1 - variaveis
   const name: string = "Eduardo";
   const age: number = 30;
@@ -34,54 +36,59 @@ function App() {
   };
 
   // 9 - type
-  const myText: textOrNull = "Tem um texto aqui"
+  const myText: textOrNull = "Tem um texto aqui";
   let mySecondText: textOrNull = null;
 
-  mySecondText = null
+  mySecondText = null;
 
-  const testandoFixed: fixed = "Isso"
+  const testandoFixed: fixed = "Isso";
 
   // 10 - context
-  const contextValue = {
+  const contextValue: IAppContext = {
     language: "Javascript",
     framework: "Express",
     projects: 5,
-  }
+  };
 
   return (
-    <div className="App">
-      <h1>React com Typescript</h1>
-      <h2>Nome: {name}</h2>
-      <p>Idade: {age}</p>
-      {isWorking && <p>Está trabalhando!</p>}
-      <h3>{userGreeting(name)}</h3>
-      {/* 4 - importando componentes */}
-      <FirstComponent />
-      {/* 5 - desestruturando props */}
-      <SecondComponent name="Segundo" />
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>React com Typescript</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking && <p>Está trabalhando!</p>}
+        <h3>{userGreeting(name)}</h3>
+        {/* 4 - importando componentes */}
+        <FirstComponent />
+        {/* 5 - desestruturando props */}
+        <SecondComponent name="Segundo" />
 
-      <Destructuring
-        title="Primeiro post"
-        content="algum conteudo..."
-        commentQty={4}
-        tags={["typescript", "javascript", "css"]}
-        category={Category.TS}
-      />
-      <Destructuring 
-        title="Segundo post"
-        content="Mais alguma coisa..."
-        commentQty={20}
-        tags={["html", "python"]}
-        category={Category.P}
-      />
+        <Destructuring
+          title="Primeiro post"
+          content="algum conteudo..."
+          commentQty={4}
+          tags={["typescript", "javascript", "css"]}
+          category={Category.TS}
+        />
+        <Destructuring
+          title="Segundo post"
+          content="Mais alguma coisa..."
+          commentQty={20}
+          tags={["html", "python"]}
+          category={Category.P}
+        />
 
-      {/* 6 - useState */}
-      <State />
+        {/* 6 - useState */}
+        <State />
 
-      {/* 9 - type */}
-      {myText && <p>Tem texto na variavel</p>}
-      {mySecondText && <p>Tem texto na variavel</p>}
-    </div>
+        {/* 9 - type */}
+        {myText && <p>Tem texto na variavel</p>}
+        {mySecondText && <p>Tem texto na variavel</p>}
+
+        {/* 10 - context */}
+        <Context />
+      </div>
+    </AppContext.Provider>
   );
 }
 
